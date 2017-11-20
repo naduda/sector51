eb52d7764a3cd5d975cd0b8b5701cfa91fe429db
pr <naduda.pr@gmail.com>
2017-11-20 16:02:12 +0200

    remove repository after install

--- docker/test/uninstall_scanner.bat
+++ docker/test/uninstall_scanner.bat
@@ -4,5 +4,9 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 call %~dp0Scanner\ScannerService.exe -u
 docker-compose -f %~dp0Scanner\docker-compose.yml down -v
-call rd /s q/ %~dp0Scanner
+docker rmi scanner_db -f
+docker rmi scanner_web -f
+docker rmi postgres -f
+docker rmi anapsix/alpine-java -f
+rd /s /q %~dp0Scanner
 pause
