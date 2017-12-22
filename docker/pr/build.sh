40dad50ef4c28de700047c6e036797e0ebd6808f
pr <naduda.pr@gmail.com>
2017-12-22 10:33:59 +0200

    *

--- docker/pr/build.sh
+++ docker/pr/build.sh
@@ -10,13 +10,11 @@ jarFile=$3
 propFile="$PWD/src/main/resources/application.properties"
 pattern="localhost:5432/sector51"
 replacement="db:5432/$4"
-echo $6
-echo $7
 sed -i -e "s|$pattern|$replacement|g" "$propFile"
 sed -i -e "s|password=12345678|password=$5|g" "$propFile"
 sed -i -e "s|user@gmail.com|$6|g" "$propFile"
 sed -i -e "s|email_password|$7|g" "$propFile"
-cat $propFile
+
 chmod +x ./gradlew
 ./gradlew build -x test
 cp -R ./build/libs/sector51server-0.0.1-SNAPSHOT.jar "$jarFile"
