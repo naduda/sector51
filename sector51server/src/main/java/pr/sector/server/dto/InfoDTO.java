c5352e4d31305725789eef45243ecc58f2908d20
NadudaP <pavlo@easyleads.com>
2019-05-09 20:30:24 +0300

    last

--- sector51server/src/main/java/pr/sector/server/dto/InfoDTO.java
+++ sector51server/src/main/java/pr/sector/server/dto/InfoDTO.java
@@ -6,9 +6,11 @@ import lombok.Data;
 public class InfoDTO {
   private final String name;
   private final String version;
+  private final String desc;
 
   public InfoDTO() {
     this.name = "sector51-server";
     this.version = "0.0.1";
+    this.desc = "last";
   }
 }
